e3ded12e8fe6c188429eb87ee37ce3fa9a73f3e1
pankaj <aiotechnicalpankaj@gmail.com>
2021-04-11 14:10:49 +0530

    implemented dynamic experience calculation and added font awesome icon

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  experience;
 
   constructor() { }
 
   ngOnInit(): void {
+    let today = new Date();
+    let joiningDate = new Date('2018-04-17');
+    let year = today.getFullYear() - joiningDate.getFullYear();
+    var m = today.getMonth() - joiningDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < joiningDate.getDate())) {
+       year--;
+    }
+    this.experience = year + '.'+m 
   }
 
 }
