c0534e238c3dfeb0b33c53c92821b05424a5954c
pankaj <aiotechnicalpankaj@gmail.com>
2021-04-11 13:12:01 +0530

    Implemented my images

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  form: FormGroup;
+  submitted: boolean;
+  success: boolean;
+  emailPattern = '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$';
+  subjectPrefix: string;
 
   constructor() { }
 
+ 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.pattern(this.emailPattern)]),
+      subject: new FormControl(null, Validators.required),
+      message: new FormControl(null, Validators.required)
+    });
   }
+
+
+  // onSubmit() {
+  //   this.submitted = true;
+
+  //   if (this.form.valid) {
+  //     this.http
+  //       .post('https://formspree.io/your@email.com', {
+  //         name: this.form.value.name,
+  //         _subject: `${this.subjectPrefix}${this.form.value.subject}`,
+  //         email: this.form.value.email,
+  //         message: this.form.value.message
+  //       })
+  //       .map(res => console.log(res.json()))
+  //       .subscribe(
+  //         success => console.log('success'),
+  //         error => console.log('error')
+  //       );
+  //   }
 
 }
